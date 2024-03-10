fa07db7f3ee43c7250ea02d990808059e989754d
kasheshjaiin <jkashish8055@gmail.com>
2024-03-10 14:28:44 -0400

    updating code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -54,13 +54,26 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Convert the query results to a dictionary."""
-    last_year_date = dt.date.today() - dt.timedelta(days=365)
-    prcp_results = session.query(measurement.date, measurement.prcp).\
-                   filter(measurement.date >= last_year_date).all()
-    
-    precipitation_data = {date: prcp for date, prcp in results}
 
-    return jsonify(prcp_dict)
+    # Starting from the most recent data point in the database. 
+    most_recent_date = dt.date(2017, 8, 23)
+    # Calculate the date one year from the last date in data set.
+    one_year_ago = most_recent_date - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    prcp_data = session.query(measurement.date, measurement.prcp).\
+    filter(measurement.date >= one_year_ago).\
+    filter(measurement.date <= most_recent_date).all()
+
+    # Create a dictionary from the row data and append to a list
+    prcp_list = []
+    for date, prcp in prcp_data:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_list.append(prcp_dict)
+
+    return jsonify(prcp_list)
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -68,7 +81,7 @@ def stations():
 
     # Query all stations
     station_results = session.query(station.station).all()
-    
+
     # Convert list of tuples into normal list
     stations = list(np.ravel(station_results))
 
@@ -77,16 +90,30 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     """Query temperature observations for the most active station for the previous year."""
+    # Query to find the most active station
     most_active_station = session.query(measurement.station, func.count(measurement.station)).\
-                          group_by(measurement.station).\
-                          order_by(func.count(measurement.station).desc()).first()[0]
+    group_by(measurement.station).\
+    order_by(func.count(measurement.station).desc()).first()
+
+    most_active_station_id = most_active_station[0]
+
+    # Starting from the most recent data point in the database. 
+    most_recent_date = dt.date(2017, 8, 23)
+    # Calculate the date one year from the last date in data set.
+    one_year_ago = most_recent_date - dt.timedelta(days=365)
     
-    last_year_date = dt.date.today() - dt.timedelta(days=365)
-    tobs_results = session.query(measurement.date, measurement.tobs).\
-                   filter(measurement.station == most_active_station).\
-                   filter(measurement.date >= last_year_date).all()
+    # Query the last 12 months of temperature observation data for this station and plot the results as a histogram
+    temperature_data = session.query(measurement.tobs).\
+    filter(measurement.station == most_active_station_id).\
+    filter(measurement.date >= one_year_ago).\
+    filter(measurement.date <= most_recent_date).all()
+
+    # Convert the query results to a list of temperatures
+    temperatures = [temp[0] for temp in temperature_data]
+
+    temp_list = list(np.ravel(temperatures))
     
-    return jsonify(tobs_results)
+    return jsonify(temp_list)
 
 @app.route("/api/v1.0/<start>")
 def start_date(start):
